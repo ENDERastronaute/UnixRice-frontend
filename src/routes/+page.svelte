37243d8820ef3ff081aafde6105f5f98460839bb
ENDERastronaute <enderastronaute@gmail.com>
2024-01-26 18:46:18 +0100

    Started development

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,27 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import Header from "./components/header/header.svelte";
+	import { IconArrowNarrowDown } from "@tabler/icons-svelte";
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Home Page</title>
+	<meta name="description" content="UnixRice Home Page" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
+<Header></Header>
+<main>
+	<section class="landing">
+		<h1>Nix4All</h1>
+		
+		<span class="scroll-icon">
+			<IconArrowNarrowDown />
 		</span>
+	</section>
+	<section class="channels">
+		
+	</section>
+</main>
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+<style lang="scss">
+	@import 'style.scss';
 </style>
---
+++ src/routes/components/header/header.svelte
@@ -0,0 +1,12 @@
+
+<header>
+    <div class="actions">
+        <a href="/login">login</a>
+        <span>/</span>
+        <a href="/signup">signup</a>
+    </div>
+</header>
+
+<style lang="scss">
+    @import "style.scss";
+</style>
